115a9c18794bee9ee4b1b35ed24f50048337f848
JP Pham <63124165+JPham-file@users.noreply.github.com>
2023-09-23 16:42:04 -0500

    added inputs in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <ctime>
+
+#include "WebServer.h"
+#include "Request.h"
+#include "LoadBalancer.h"
+
+using std::cout, std::endl, std::cin;
+using std::srand;
+using std::queue;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // TODO: user input for numbers of servers and simulation time
+    int num_servers = 0;
+    int num_time = 0;
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    cout << "number of servers: ";
+    cin >> num_servers;
+    cout << "number of time: ";
+    cin >> num_time;
+    cout << "[STARTING]" << endl;
+
+//    queue<Request> request_q;
+
+    // TODO: TESTING
+    queue<int> request_q;
+    int request_q_size = num_servers*5;
+    for (int i = 0; i < request_q_size; i++) {
+        request_q.push(i);
+    }
+
+
+    cout << "[ENDED]" << endl;
     return 0;
 }
